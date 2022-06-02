361b0fe836844a24e145f197227272431cd16647
Saba Chikhinashvili <sabachikhinashvili@Sabas-MacBook-Pro.local>
2022-06-02 12:47:26 +0400

    fix: use try on outputs for handling errors

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "eni_id" {
   description = "ID of the ENI for the NAT instance"
-  value       = aws_network_interface.main[0].id
+  value       = try(aws_network_interface.main[0].id, "")
 }
 
 output "eni_private_ip" {
   description = "Private IP of the ENI for the NAT instance"
-  value = tolist(aws_network_interface.main[0].private_ips)[0]
+  value = try(tolist(aws_network_interface.main[0].private_ips)[0], "")
 }
 
 output "sg_id" {
   description = "ID of the security group of the NAT instance"
-  value       = aws_security_group.main[0].id
+  value       = try(aws_security_group.main[0].id, "")
 }
 
 output "iam_role_name" {
   description = "Name of the IAM role for the NAT instance"
-  value       = aws_iam_role.main[0].name
+  value       = try(aws_iam_role.main[0].name, "")
 }
